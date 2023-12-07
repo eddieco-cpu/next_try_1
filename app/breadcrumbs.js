d0a955b9b9f5abadc817566959bc38b511b15a58
udnlinyi <130973011+udnlinyi@users.noreply.github.com>
2023-12-07 16:00:57 +0800

    try process.env.TEST_TEXT 2

--- app/breadcrumbs.js
+++ app/breadcrumbs.js
@@ -3,18 +3,21 @@
 import Mainfield from "./(ui)/mainFieldset";
 import { usePathname } from "next/navigation";
 
+const envVar = process.env.TEST_TEXT || "no env";
+
 //
 function Breadcrumbs() {
   //
   const pathname = usePathname();
 
   console.log(pathname);
-  console.log(process.env.TEST_TEXT || "no env");
+  //console.log(process.env.TEST_TEXT || "no env");
 
   return (
     <div className="breadcrumbs">
       <Mainfield {...{ className: "", legend: "breadcrumbs" }}>
         <p>{pathname.toString()}</p>
+        <b>{envVar}</b>
       </Mainfield>
     </div>
   );
