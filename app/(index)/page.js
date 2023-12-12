f2f588dcaf66f5db8435a024baa0cc32337a42b9
udnlinyi <130973011+udnlinyi@users.noreply.github.com>
2023-12-12 19:34:36 +0800

    Add .env file and display environment variable in HomePage

--- app/(index)/page.js
+++ app/(index)/page.js
@@ -1,11 +1,14 @@
 //import React from "react";
 import styles from "./page.module.css";
 
+const envVar = process.env.TEST_TEXT || "no env";
+
 const HomePage = () => {
   return (
     <div>
       <h1>Welcome to my app!</h1>
       <p>This is the home page.</p>
+      <p>env: {envVar}</p>
     </div>
   );
 };
